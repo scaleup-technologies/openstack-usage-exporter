46473ec764b06592f70dd43afe40d3fdd870361e
Freerk-Ole Zakfeld <fzakfeld@scaleuptech.com>
2024-11-20 14:36:47 +0100

    Add Manila Backups + Snapshots

--- exporters/manila.go
+++ exporters/manila.go
@@ -8,8 +8,10 @@ import (
 )
 
 type ManilaUsageExporter struct {
-	db         *sql.DB
-	sharesSize *prometheus.Desc
+	db                 *sql.DB
+	sharesSize         *prometheus.Desc
+	shareSnapshotsSize *prometheus.Desc
+	shareBackupsSize   *prometheus.Desc
 }
 
 func NewManilaUsageExporter(db *sql.DB) (*ManilaUsageExporter, error) {
@@ -20,18 +22,32 @@ func NewManilaUsageExporter(db *sql.DB) (*ManilaUsageExporter, error) {
 			"Total share size in GB per OpenStack project",
 			[]string{"project_id"}, nil,
 		),
+		shareSnapshotsSize: prometheus.NewDesc(
+			"openstack_project_share_snapshots_size_gb",
+			"Total share snapshot size in GB per OpenStack project",
+			[]string{"project_id"}, nil,
+		),
+		shareBackupsSize: prometheus.NewDesc(
+			"openstack_project_share_backups_size_gb",
+			"Total share backup size in GB per OpenStack project",
+			[]string{"project_id"}, nil,
+		),
 	}, nil
 }
 
 func (e *ManilaUsageExporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- e.sharesSize
+	ch <- e.shareSnapshotsSize
+	ch <- e.shareBackupsSize
 }
 
 func (e *ManilaUsageExporter) Collect(ch chan<- prometheus.Metric) {
-	e.collectMetrics(ch)
+	e.collectShareSize(ch)
+	e.collectShareSnapshotSize(ch)
+	e.collectShareBackupSize(ch)
 }
 
-func (e *ManilaUsageExporter) collectMetrics(ch chan<- prometheus.Metric) {
+func (e *ManilaUsageExporter) collectShareSize(ch chan<- prometheus.Metric) {
 	rows, err := e.db.Query("SELECT project_id, SUM(size) AS shares_size FROM shares WHERE deleted='False' GROUP BY project_id")
 	if err != nil {
 		log.Println("Error querying Manila database:", err)
@@ -51,6 +67,64 @@ func (e *ManilaUsageExporter) collectMetrics(ch chan<- prometheus.Metric) {
 			e.sharesSize,
 			prometheus.GaugeValue,
 			sharesSize,
+			projectID,
+		)
+
+	}
+	if err := rows.Err(); err != nil {
+		log.Println("Error in Manila result set:", err)
+	}
+}
+
+func (e *ManilaUsageExporter) collectShareSnapshotSize(ch chan<- prometheus.Metric) {
+	rows, err := e.db.Query("SELECT project_id, SUM(size) AS share_snapshots_size FROM share_snapshots WHERE deleted='False' GROUP BY project_id")
+	if err != nil {
+		log.Println("Error querying Manila database:", err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var projectID string
+		var shareSnapshotsSize float64
+		if err := rows.Scan(&projectID, &shareSnapshotsSize); err != nil {
+			log.Println("Error scanning Manila row:", err)
+			continue
+		}
+
+		ch <- prometheus.MustNewConstMetric(
+			e.shareSnapshotsSize,
+			prometheus.GaugeValue,
+			shareSnapshotsSize,
+			projectID,
+		)
+
+	}
+	if err := rows.Err(); err != nil {
+		log.Println("Error in Manila result set:", err)
+	}
+}
+
+func (e *ManilaUsageExporter) collectShareBackupSize(ch chan<- prometheus.Metric) {
+	rows, err := e.db.Query("SELECT project_id, SUM(size) AS share_backups_size FROM share_backups WHERE deleted='False' GROUP BY project_id")
+	if err != nil {
+		log.Println("Error querying Manila database:", err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var projectID string
+		var shareBackupsSize float64
+		if err := rows.Scan(&projectID, &shareBackupsSize); err != nil {
+			log.Println("Error scanning Manila row:", err)
+			continue
+		}
+
+		ch <- prometheus.MustNewConstMetric(
+			e.shareBackupsSize,
+			prometheus.GaugeValue,
+			shareBackupsSize,
 			projectID,
 		)
 
--- exporters/manila_test.go
+++ exporters/manila_test.go
@@ -1,7 +1,6 @@
 package exporters
 
 import (
-	"regexp"
 	"strings"
 	"testing"
 
@@ -20,8 +19,19 @@ func TestManilaUsageExporter(t *testing.T) {
 		"project_id", "shares_size"}).
 		AddRow("6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096", 2).
 		AddRow("c352b0ed-30ca-4634-9c2d-1947efc29096", 12)
-	mock.ExpectQuery(regexp.QuoteMeta("SELECT project_id, SUM")).
-		WillReturnRows(sharesRows)
+	mock.ExpectQuery("SELECT project_id, SUM\\(size\\) AS shares_size FROM shares").WillReturnRows(sharesRows)
+
+	shareSnapshotsRows := sqlmock.NewRows([]string{
+		"project_id", "shares_size"}).
+		AddRow("6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096", 4).
+		AddRow("c352b0ed-30ca-4634-9c2d-1947efc29096", 24)
+	mock.ExpectQuery("SELECT project_id, SUM\\(size\\) AS share_snapshots_size FROM share_snapshots").WillReturnRows(shareSnapshotsRows)
+
+	shareBackupsRows := sqlmock.NewRows([]string{
+		"project_id", "shares_size"}).
+		AddRow("6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096", 1).
+		AddRow("c352b0ed-30ca-4634-9c2d-1947efc29096", 0)
+	mock.ExpectQuery("SELECT project_id, SUM\\(size\\) AS share_backups_size FROM share_backups").WillReturnRows(shareBackupsRows)
 
 	exporter, err := NewManilaUsageExporter(db)
 	if err != nil {
@@ -29,6 +39,14 @@ func TestManilaUsageExporter(t *testing.T) {
 	}
 
 	expectedMetrics := `
+        # HELP openstack_project_share_backups_size_gb Total share backup size in GB per OpenStack project
+        # TYPE openstack_project_share_backups_size_gb gauge
+        openstack_project_share_backups_size_gb{project_id="6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096"} 1
+        openstack_project_share_backups_size_gb{project_id="c352b0ed-30ca-4634-9c2d-1947efc29096"} 0
+        # HELP openstack_project_share_snapshots_size_gb Total share snapshot size in GB per OpenStack project
+        # TYPE openstack_project_share_snapshots_size_gb gauge
+        openstack_project_share_snapshots_size_gb{project_id="6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096"} 4
+        openstack_project_share_snapshots_size_gb{project_id="c352b0ed-30ca-4634-9c2d-1947efc29096"} 24
         # HELP openstack_project_shares_size_gb Total share size in GB per OpenStack project
         # TYPE openstack_project_shares_size_gb gauge
         openstack_project_shares_size_gb{project_id="6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096"} 2
