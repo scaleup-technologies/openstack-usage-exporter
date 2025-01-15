6ac58e4300fec5af1a19ca05aa2403a7ec44c3dd
Freerk-Ole Zakfeld <43890789+fzakfeld@users.noreply.github.com>
2025-01-15 15:46:00 +0100

    Add Nova Trait exporter (#5)

---
+++ exporters/nova_trait.go
@@ -0,0 +1,78 @@
+package exporters
+
+import (
+	"database/sql"
+	"log"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type NovaTraitUsageExporter struct {
+	db        *sql.DB
+	trait     string
+	vcpus     *prometheus.Desc
+	instances *prometheus.Desc
+}
+
+func NewNovaTraitUsageExporter(db *sql.DB, trait string) (*NovaTraitUsageExporter, error) {
+	return &NovaTraitUsageExporter{
+		db:    db,
+		trait: trait,
+		vcpus: prometheus.NewDesc(
+			"openstack_project_vcpus_trait__"+strings.ToLower(trait),
+			"Total number of vcpus per OpenStack project for instances with image trait "+trait,
+			[]string{"project_id"}, nil,
+		),
+		instances: prometheus.NewDesc(
+			"openstack_project_instances_trait__"+strings.ToLower(trait),
+			"Total number of instances per OpenStack project with image trait "+trait,
+			[]string{"project_id"}, nil,
+		),
+	}, nil
+}
+
+func (e *NovaTraitUsageExporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- e.vcpus
+	ch <- e.instances
+}
+
+func (e *NovaTraitUsageExporter) Collect(ch chan<- prometheus.Metric) {
+	e.collectMetrics(ch)
+}
+
+func (e *NovaTraitUsageExporter) collectMetrics(ch chan<- prometheus.Metric) {
+	rows, err := e.db.Query("SELECT i.project_id AS project_id, COUNT(i.id) AS total_instances, SUM(vcpus) AS total_vcpus FROM instances i INNER JOIN instance_system_metadata m on i.uuid = m.instance_uuid WHERE i.deleted = 0 AND m.key = ? and m.value = 'required' GROUP BY project_id", "image_trait:"+e.trait)
+	if err != nil {
+		log.Println("Error querying Nova database:", err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var projectID string
+		var totalVcpus float64
+		var totalInstances float64
+		if err := rows.Scan(&projectID, &totalVcpus, &totalInstances); err != nil {
+			log.Println("Error scanning Nova row:", err)
+			continue
+		}
+
+		ch <- prometheus.MustNewConstMetric(
+			e.vcpus,
+			prometheus.GaugeValue,
+			totalVcpus,
+			projectID,
+		)
+
+		ch <- prometheus.MustNewConstMetric(
+			e.instances,
+			prometheus.GaugeValue,
+			totalInstances,
+			projectID,
+		)
+	}
+	if err := rows.Err(); err != nil {
+		log.Println("Error in Nova result set:", err)
+	}
+}
---
+++ exporters/nova_trait_test.go
@@ -0,0 +1,47 @@
+package exporters
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestNovaTraitUsageExporter(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Failed to create sqlmock: %v", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"project_id", "total_instances", "total_vcpus"}).
+		AddRow("c352b0ed-30ca-4634-9c2d-1947efc29096", 0, 0).
+		AddRow("6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096", 4, 5)
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+
+	exporter, err := NewNovaTraitUsageExporter(db, "CUSTOM_TRAIT")
+	if err != nil {
+		t.Fatalf("Failed to create NewNovaTraitUsageExporter: %v", err)
+	}
+
+	expectedMetrics := `
+        # HELP openstack_project_instances_trait__custom_trait Total number of instances per OpenStack project with image trait CUSTOM_TRAIT
+        # TYPE openstack_project_instances_trait__custom_trait gauge
+        openstack_project_instances_trait__custom_trait{project_id="6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096"} 5
+        openstack_project_instances_trait__custom_trait{project_id="c352b0ed-30ca-4634-9c2d-1947efc29096"} 0
+        # HELP openstack_project_vcpus_trait__custom_trait Total number of vcpus per OpenStack project for instances with image trait CUSTOM_TRAIT
+        # TYPE openstack_project_vcpus_trait__custom_trait gauge
+        openstack_project_vcpus_trait__custom_trait{project_id="6ee08ba2-2ca1-4c91-b139-4bf0dbaa4096"} 4
+        openstack_project_vcpus_trait__custom_trait{project_id="c352b0ed-30ca-4634-9c2d-1947efc29096"} 0
+
+	`
+
+	if err := testutil.CollectAndCompare(exporter, strings.NewReader(expectedMetrics)); err != nil {
+		t.Errorf("unexpected collecting result:\n%s", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("There were unfulfilled expectations: %s", err)
+	}
+}
